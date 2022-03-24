800cb52fd5ff05da0d41b01ec91d6d1ee423682e
Janaina Paula <jana@cora.com.br>
2022-03-24 11:19:36 -0300

    Criando endpoint para cadastrar cliente

---
+++ src/main/kotlin/br/com/janadev/customer/primary/CustomerControler.kt
@@ -0,0 +1,27 @@
+package br.com.janadev.customer.primary
+
+import br.com.janadev.customer.domain.CustomerPort
+import br.com.janadev.customer.domain.toDTO
+import br.com.janadev.customer.primary.dto.CustomerResponseDTO
+import br.com.janadev.customer.primary.dto.NewCustomerDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+
+@RestController
+@RequestMapping("/api/customer")
+class CustomerControler(
+    val customerPort: CustomerPort
+) {
+
+    @PostMapping
+    fun createCustomer(@RequestBody customer: NewCustomerDTO): ResponseEntity<CustomerResponseDTO> {
+        val customerCreated = customerPort.createCustomer(customer.toModel()).toDTO()
+        val uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}").buildAndExpand(customerCreated.id).toUri()
+        return ResponseEntity.created(uri).build()
+    }
+}
